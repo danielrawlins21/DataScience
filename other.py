50a0bdc0adbdca15320ce8f2739fcb180362eb3f
daniel-rawlins <daniel.rawlins1@utp.ac.pa>
2023-09-12 21:07:47 -0500

    Debugging test

--- other.py
+++ other.py
@@ -1,22 +1,19 @@
 def get_sum_metrics(predictions, metrics=None):
-    #breakpoint()
     if metrics is None:
         metrics = []
-    
-    
+
+    def create_sum(i):
+        return lambda x: x + i
+
     for i in range(3):
-        
-        def create_sum(i):
-            return [lambda x: x + i ]
         metrics.append(create_sum(i))
-    
+
     sum_metrics = 0
     for metric in metrics:
         breakpoint()
         sum_metrics += metric(predictions)
-    breakpoint()
-    return sum_metrics
 
+    return sum_metrics
 
 def main():
     print(get_sum_metrics(0))  # Should be (0 + 0) + (0 + 1) + (0 + 2) = 3
