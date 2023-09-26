beb0b390ec6c7b7207f212162acd2a522fd10e97
Daniel Rawlins <62584162+danielrawlins21@users.noreply.github.com>
2023-09-26 20:01:31 +0000

    Commit with part 7

--- project1/sentiment_analysis/project1.py
+++ project1/sentiment_analysis/project1.py
@@ -387,7 +387,22 @@ def classifier_accuracy(
     """
     # Your code here
     #Training with feature matrix
-    (theta, theta_0) =
+    (theta, theta_0) = classifier(
+        train_feature_matrix,
+        train_labels,
+        **kwargs
+    )
+    #Train with classify function
+    t_labels = classify(train_feature_matrix, theta, theta_0)
+    t_accur = accuracy(t_labels, train_labels)
+    print(t_accur)
+
+    #validation classify
+    v_labels = classify(val_feature_matrix, theta, theta_0)
+    v_acurr = accuracy(v_labels, val_labels)
+    print(v_acurr)
+
+    return(t_accur,v_acurr)
 
 
 
@@ -401,7 +416,7 @@ def extract_words(text):
         count as their own words.
     """
     # Your code here
-    raise NotImplementedError
+    #raise NotImplementedError
 
     for c in punctuation + digits:
         text = text.replace(c, ' ' + c + ' ')
@@ -421,11 +436,17 @@ def bag_of_words(texts, remove_stopword=False):
         integer `index`.
     """
     # Your code here
-    raise NotImplementedError
+    #raise NotImplementedError
     
     indices_by_word = {}  # maps word to unique index
     for text in texts:
         word_list = extract_words(text)
+        if remove_stopword == False:
+            stopword = []
+        else:
+            ##### Something you need to implement for Part 9
+            ##### Define what stopword is
+            stopword = []
         for word in word_list:
             if word in indices_by_word: continue
             if word in stopword: continue
@@ -454,7 +475,8 @@ def extract_bow_feature_vectors(reviews, indices_by_word, binarize=True):
             feature_matrix[i, indices_by_word[word]] += 1
     if binarize:
         # Your code here
-        raise NotImplementedError
+        feature_matrix[feature_matrix > 0] = 1
+      
     return feature_matrix
 
 
