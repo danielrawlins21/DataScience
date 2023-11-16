c410ef44c76ab9f023e8ae8a4c1449a14bc5ea40
daniel-rawlins <daniel.rawlins1@utp.ac.pa>
2023-11-16 14:41:46 -0500

    Kmeans first edit

--- netflix/main.py
+++ netflix/main.py
@@ -5,5 +5,22 @@ import naive_em
 import em
 
 X = np.loadtxt("toy_data.txt")
+K = [1,2,3,4]
 
-# TODO: Your code here
+
+#Kmeans parameters init
+costs_Kmeans = [0,0,0,0,0]
+best_seed_Kmeans = [0,0,0,0,0]
+mixtures_Kmeans = [0,0,0,0,0]
+posts_Kmeans = [0,0,0,0,0]
+
+
+for k in K:
+    for seed in [0,1,2,3,4]:
+        init_model = common.init(X,k,seed)
+
+        kmeans_m = kmeans.run(X,k,init_model)
+    print("|||||||| Clusters-",k," ||||||||")
+    print("Lowest cost using Kmeans is:",np.min(costs_Kmeans))
+        
+
